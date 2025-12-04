8100d6be65228b4bc1073346b023a8880a80f16e
myLessonss <izabayoeloi2@gmail.com>
2025-12-04 18:55:16 +0200

    Hospital Sysytem

--- Studying/Department.java
+++ Studying/Department.java
@@ -14,4 +14,20 @@ public class Department extends Hospital{
     public String getdepartmentName(){
         return departmentName;
     }
+    public String getdepartmentCode(){
+        return departmentCode;
+    }
+    //Setters 
+    public void setdepartmentName(String departmentName){
+        if (departmentName==null) {
+            throw new IllegalArgumentException("Department name must not be Empty");
+        }
+        this.departmentName=departmentName;
+    }
+    public void setdepartmentCode(String departmentCode){
+        if (!departmentCode.matches("[A-Za-z0-9]+")||departmentCode.length()<=3) {
+            
+        }
+        this.departmentCode=departmentCode;
+    }
 }
