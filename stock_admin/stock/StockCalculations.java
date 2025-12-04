e02f6cb9acb2e052067bc386833b08d360059977
27131 <washimoney2@gmail.com>
2025-12-04 19:03:38 +0200

    Delete stock_admin/stock directory

--- stock_admin/stock/StockCalculations.java
+++
@@ -1,7 +0,0 @@
-package stock;
-interface StockCalculations{
-    double calculateVat(double turnoverAmount);
-    double markupProfit(double sellingPrice,double buyingPrice);
-    double totalStockValue(int quantity,double sellingPrice);
-    double restockingFee(double buyingPrice);
-}
